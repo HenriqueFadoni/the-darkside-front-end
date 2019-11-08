6f1b2593ed0c2d0bd37b77859450afa077d421d7
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-11-08 17:04:39 -0300

    Creating Footer

--- src/containers/LoginPage/LoginPage.tsx
+++ src/containers/LoginPage/LoginPage.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import TopLogo from './TopLogo/TopLogo';
 import Form from './Form/Form';
+import Footer from './Footer/Footer';
 
 const LoginPage: React.FC = () => {
   return (
     <div className="login-page__container">
       <TopLogo />
       <Form />
+      <Footer />
     </div>
   );
 }
