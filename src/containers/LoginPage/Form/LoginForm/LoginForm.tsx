b1e0ee8fc53e2c15b0df5dff32768163934b3429
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-11-08 15:00:09 -0300

    Styling Inputs and Submit Button

--- src/containers/LoginPage/Form/LoginForm/LoginForm.tsx
+++ src/containers/LoginPage/Form/LoginForm/LoginForm.tsx
@@ -3,12 +3,33 @@ import React from 'react';
 const LoginForm: React.FC = () => {
   return (
     <div>
-      <form>
-        <input type="email" placeholder="E-mail" required/>
-        <input type="password" placeholder="Password" required/>
-        <button type="submit">LOGIN</button>
+      <form className="login-page__form--fields">
+        <input 
+          className="login-page__form--inputs" 
+          type="email" 
+          placeholder="E-mail" 
+          required
+        />
+        <input 
+          className="login-page__form--inputs" 
+          type="password" 
+          placeholder="Password" 
+          required
+        />
+        <button 
+          className="login-page__form--submit-btn"
+          type="submit"
+        >
+          LOGIN
+        </button>
       </form>
-      <p className="arciform body-text-2">Doesn't Have an Account? <span>Create Here!</span></p>
+
+      <p className="arciform body-text-2 text-white">
+        Doesn't Have an Account?  
+        <span className="text-purple margin-left-05">
+          Create Here!
+        </span>
+      </p>
     </div>
   );
 }
