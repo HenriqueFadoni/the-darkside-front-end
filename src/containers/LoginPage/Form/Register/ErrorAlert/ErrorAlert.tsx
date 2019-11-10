06126bed6de395cabb6c6b54a7b2160cb410fcd1
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-11-09 22:48:42 -0300

    ErrorHandling in Register

---
+++ src/containers/LoginPage/Form/Register/ErrorAlert/ErrorAlert.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorAlert = () => (
+  <p className="arciform body-text-1 text-center text-red">
+    Email already registered
+  </p>
+)
+
+export default ErrorAlert;
--- src/containers/LoginPage/Form/Register/Register.tsx
+++ src/containers/LoginPage/Form/Register/Register.tsx
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 import RegisterForm from './RegisterForm/RegisterForm';
 import CallForAction from '../../../../components/CallForAction/CallForAction';
+import ErrorAlert from './ErrorAlert/ErrorAlert';
 
 interface RegisterProps {
   signInHandler: () => void;
 }
 
 const Register: React.FC<RegisterProps> = ({ signInHandler }) => {
+  const [error, setError] = useState(false);
+
   const onSubmitHandler = async (email: string, password: string) => {
-    const key = process.env.REACT_APP_FIREBASE_API_KEY;
-    const path = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`
-    const subsData = {
-      email: email,
-      password: password,
-      returnSecureToken: true
+    try {
+      const key = process.env.REACT_APP_FIREBASE_API_KEY;
+      const path = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`
+      const subsData = {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+      const { data } = await axios.post(path, subsData);
+
+    } catch (error) {
+      setError(true)
     }
-    const { data } = await axios.post(path, subsData);
   }
+
+  const errorHandler = (
+    <ErrorAlert />
+  )
 
   return (
     <div>
+      {error && errorHandler}
       <RegisterForm onSubmitHandler={onSubmitHandler} />
       <CallForAction
         question='Already Have an Account?'
