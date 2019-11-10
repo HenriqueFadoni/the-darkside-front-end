f01e2d82c48e12daabab19cff36037e95a2af085
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-11-09 22:29:26 -0300

    Validating Fields in Register

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 interface FormInputProps {
   inputType: string,
   name: string,
-  holder: string
+  holder: string,
+  isIncorrect?: boolean,
   onChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const FormInput: React.FC<FormInputProps> = ({ inputType, name, holder, onChangeHandler }) => (
+const FormInput: React.FC<FormInputProps> = ({ inputType, name, holder, onChangeHandler, isIncorrect = false }) => (
   <input
-    className="form--inputs"
+    className={`form--inputs ${isIncorrect && 'form--incorrect'}`}
     type={inputType}
     name={name}
     onChange={onChangeHandler}
--- src/containers/LoginPage/Form/Register/RegisterForm/RegisterForm.tsx
+++ src/containers/LoginPage/Form/Register/RegisterForm/RegisterForm.tsx
@@ -11,25 +11,51 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ onSubmitHandler }) => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [secondPassword, setSecondPassword] = useState();
+  const [incorrectEmail, setincorrectEmail] = useState(false);
   const [incorrectPass, setincorrectPass] = useState(false);
 
   const onEmailChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+    const newEmail = e.target.value;
+
+    if (newEmail.trim() === '' || !newEmail.includes('@')) {
+      setincorrectEmail(true);
+    } else if (incorrectEmail) {
+      setincorrectEmail(false);
+    }
+
+    setEmail(newEmail);
   }
 
   const onPasswordChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+    const newPassword = e.target.value;
+
+    if (newPassword.trim() === '') {
+      setincorrectPass(true);
+    } else if (newPassword === secondPassword) {
+      setincorrectPass(false);
+    }
+
+    setPassword(newPassword);
   }
 
   const onSecondPasswordChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSecondPassword(e.target.value);
+    const newSecondPassword = e.target.value;
+
+    if (newSecondPassword.trim() === '' || password !== newSecondPassword) {
+      setincorrectPass(true);
+    } else if (password === newSecondPassword) {
+      setincorrectPass(false);
+    }
+
+    setSecondPassword(newSecondPassword);
   }
 
   const onSubmitForm = (e: any) => {
     e.preventDefault();
     if (password === secondPassword) {
+      setincorrectPass(false);
       onSubmitHandler(email, password);
-    } else if (password !== secondPassword){
+    } else if (password !== secondPassword) {
       setincorrectPass(true);
     }
   }
@@ -44,18 +70,21 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ onSubmitHandler }) => {
         name="email"
         holder="E-mail"
         onChangeHandler={onEmailChangeHandler}
+        isIncorrect={incorrectEmail}
       />
       <FormInput
         inputType="password"
         name="password"
         holder="Password"
         onChangeHandler={onPasswordChangeHandler}
+        isIncorrect={incorrectPass}
       />
       <FormInput
         inputType="password"
         name="password-repeat"
         holder="Password"
         onChangeHandler={onSecondPasswordChangeHandler}
+        isIncorrect={incorrectPass}
       />
       <FormBtn text="REGISTER" />
     </form>
